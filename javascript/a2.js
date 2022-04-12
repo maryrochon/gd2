984f357fff1c47921af4eb91dc56d294fda5e4bf
maryrochon <99832210+maryrochon@users.noreply.github.com>
2022-04-12 09:44:15 -0400

    Add files via upload

---
+++ javascript/a2.js
@@ -0,0 +1,68 @@
+//menu bar
+
+$(document).mousemove(function(){
+    if($(".item1:hover").length == 1) {
+        $(".item1").css("background", "purple")
+        $("#menu1").css("font-size", "25px")
+        $("#menu1").css("letter-spacing", "10px") }
+    else {
+        $(".item1").css("background", "rgb(204, 166, 253)")
+        $("#menu1").css("font-size", "20px")
+        $("#menu1").css("letter-spacing", "normal")}
+
+    if($(".item2:hover").length == 1) {
+        $(".item2").css("background", "purple")
+        $("#menu2").css("font-size", "25px")
+        $("#menu2").css("letter-spacing", "10px")}
+    else {
+        $(".item2").css("background", "rgb(204, 166, 253)")
+        $("#menu2").css("font-size", "20px")
+        $("#menu2").css("letter-spacing", "normal")}
+
+    if($(".item3:hover").length == 1) {
+        $(".item3").css("background", "purple")
+        $("#menu3").css("font-size", "25px")
+        $("#menu3").css("letter-spacing", "10px")}
+    else {
+        $(".item3").css("background", "rgb(204, 166, 253)")
+        $("#menu3").css("font-size", "20px")
+        $("#menu3").css("letter-spacing", "normal")}
+
+    if($(".item4:hover").length == 1) {
+        $(".item4").css("background", "purple")
+        $("#menu4").css("font-size", "25px")
+        $("#menu4").css("letter-spacing", "10px")}
+    else {
+        $(".item4").css("background", "rgb(204, 166, 253)")
+        $("#menu4").css("font-size", "20px")
+        $("#menu4").css("letter-spacing", "normal")}
+
+    if($(".item5:hover").length == 1) {
+        $(".item5").css("background", "purple")
+        $("#menu5").css("font-size", "25px")
+        $("#menu5").css("letter-spacing", "10px")}
+    else {
+        $(".item5").css("background", "rgb(204, 166, 253)")
+        $("#menu5").css("font-size", "20px")
+        $("#menu5").css("letter-spacing", "normal")}
+
+    if($(".item6:hover").length == 1) {
+        $(".item6").css("background", "purple")
+        $("#menu6").css("font-size", "25px")
+        $("#menu6").css("letter-spacing", "10px")}
+    else {
+        $(".item6").css("background", "rgb(204, 166, 253)")
+        $("#menu6").css("font-size", "20px")
+        $("#menu6").css("letter-spacing", "normal")}
+
+    if($(".item7:hover").length == 1) {
+        $(".item7").css("background", "purple")
+        $("#menu7").css("font-size", "25px")
+        $("#menu7").css("letter-spacing", "10px")
+    }
+    else {
+        $(".item7").css("background", "rgb(204, 166, 253)")
+        $("#menu7").css("font-size", "20px")
+        $("#menu7").css("letter-spacing", "normal")}
+})
+alert ("Thank you for visiting my assignment 2 page! :) Just a heads up this page draws from outside sources so it will take a minute for it to fully load after you dismiss this alert.")
---
+++ javascript/rd.js
@@ -0,0 +1,66 @@
+//menu bar
+$(document).mousemove(function(){
+    if($(".item1:hover").length == 1) {
+        $(".item1").css("background", "purple")
+        $("#title1").css("font-size", "25px")
+        $("#title1").css("letter-spacing", "10px") }
+    else {
+        $(".item1").css("background", "rgb(204, 166, 253)")
+        $("#title1").css("font-size", "20px")
+        $("#title1").css("letter-spacing", "normal")}
+
+    if($(".item2:hover").length == 1) {
+        $(".item2").css("background", "purple")
+        $("#title2").css("font-size", "25px")
+        $("#title2").css("letter-spacing", "10px")}
+    else {
+        $(".item2").css("background", "rgb(204, 166, 253)")
+        $("#title2").css("font-size", "20px")
+        $("#title2").css("letter-spacing", "normal")}
+
+    if($(".item3:hover").length == 1) {
+        $(".item3").css("background", "purple")
+        $("#title3").css("font-size", "25px")
+        $("#title3").css("letter-spacing", "10px")}
+    else {
+        $(".item3").css("background", "rgb(204, 166, 253)")
+        $("#title3").css("font-size", "20px")
+        $("#title3").css("letter-spacing", "normal")}
+
+    if($(".item4:hover").length == 1) {
+        $(".item4").css("background", "purple")
+        $("#title4").css("font-size", "25px")
+        $("#title4").css("letter-spacing", "10px")}
+    else {
+        $(".item4").css("background", "rgb(204, 166, 253)")
+        $("#title4").css("font-size", "20px")
+        $("#title4").css("letter-spacing", "normal")}
+
+    if($(".item5:hover").length == 1) {
+        $(".item5").css("background", "purple")
+        $("#title5").css("font-size", "25px")
+        $("#title5").css("letter-spacing", "10px")}
+    else {
+        $(".item5").css("background", "rgb(204, 166, 253)")
+        $("#title5").css("font-size", "20px")
+        $("#title5").css("letter-spacing", "normal")}
+
+    if($(".item6:hover").length == 1) {
+        $(".item6").css("background", "purple")
+        $("#title6").css("font-size", "25px")
+        $("#title6").css("letter-spacing", "10px")}
+    else {
+        $(".item6").css("background", "rgb(204, 166, 253)")
+        $("#title6").css("font-size", "20px")
+        $("#title6").css("letter-spacing", "normal")}
+
+    if($(".item7:hover").length == 1) {
+        $(".item7").css("background", "purple")
+        $("#title7").css("font-size", "25px")
+        $("#title7").css("letter-spacing", "10px")
+    }
+    else {
+        $(".item7").css("background", "rgb(204, 166, 253)")
+        $("#title7").css("font-size", "20px")
+        $("#title7").css("letter-spacing", "normal")}
+})
